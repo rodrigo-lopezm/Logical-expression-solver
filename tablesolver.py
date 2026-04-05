242ecdeb16c01ca33e400838d65eaaeb105324f3
rodrigo-lopezm <rodrigometrolm@gmail.com>
2026-04-05 13:18:55 +0200

    Started tokenization implementation to the main program

--- tablesolver.py
+++ tablesolver.py
@@ -10,67 +10,76 @@ Operators = ["(", ")", "&", "+", "!"]
 SubVars = ["1","0"]
 Tokens = []
 Vars = []
-VarCounter = 0
-expression = ""
+expression = "a & b + !C + D"
 
 
-
-def Passer(mode):
-    global Operators; global VarCounter; global Tokens; global Vars; global expression; global SubVars
-    if mode == "i":
-        Depth = 0
-        Before = ""
-        for char in expression:
-            if Before == "" and char in ["&", "+", ")"]:
-                print("Wrong format: Equation starts with operator")
-                return False
-            #Checks
-            print(Before, char)
-            print(Before.isalpha(),char.isalpha())
-
-            if char not in Operators and not char.isalpha() and char not in SubVars:
-                print("That is an invalid equation.")
-                return False
-            elif char.isalpha() or char in SubVars :
-                if Before.isalpha() or Before in SubVars:
-                    print("Wrong format: Two consecutive variables")
-                    return False
-                elif char.upper() not in Vars and char.isalpha():
-                    VarCounter +=1
-                    if VarCounter <= 6:
-                        Vars.append(char.upper())
-                    else:
-                        print("There is a limit of 6 boolean variables.")
-                        return False
-            else:
-                if char == "(":
-                    if Before.isalpha() or Before in SubVars:
-                        print("Wrong format")
-                        return False
-                    else:
-                        Depth+=1
-                elif char == ")":
-                    if Before in ["&", "+"]:
-                        print("Wrong format")
-                        return False
-                    else:
-                        Depth -= 1
-                elif char == "!":
-                    if Before.isalpha() or Before in SubVars:
-                        print("Wrong format")
-                        return False
-                else:
-                    if Before in ["&","+","!","("]:
-                        print("Worng format")
-                        return False
-
-            Before = char
-
-        if Depth != 0 or Before in ["&","+","!","("]:
-            print("Wrong format: incomplete expression")
+def Passer():
+    global Operators; global Tokens; global Vars; global expression; global SubVars
+    #Mode i is the same as "initial pass"
+    Tokens = []
+    Depth = 0
+    Before = ""
+    for char in expression:
+        #Checks for equation validity
+        if char == " ":
+            continue
+        if Before == "" and char in ["&", "+", ")"]:
+            print("Wrong format: Equation starts with operator")
             return False
+        if char not in Operators and not char.isalpha() and char not in SubVars:
+            print("That is an invalid equation.")
+            return False
+        elif char.isalpha() or char in SubVars :
+            if Before.isalpha() or Before in SubVars:
+                print("Wrong format: Two consecutive variables")
+                return False
+            elif char.upper() not in Vars and char.isalpha():
+                if len(Vars) <= 6:
+                    Vars.append(char.upper())
+                else:
+                    print("There is a limit of 6 boolean variables.")
+                    return False
         else:
-            return True
+            if char == "(":
+                if Before.isalpha() or Before in SubVars:
+                    print("Wrong format")
+                    return False
+                else:
+                    Depth+=1
+            elif char == ")":
+                if Before in ["&", "+"]:
+                    print("Wrong format")
+                    return False
+                else:
+                    Depth -= 1
+            elif char == "!":
+                if Before.isalpha() or Before in SubVars:
+                    print("Wrong format")
+                    return False
+            else:
+                if Before in ["&","+","!","("]:
+                    print("Worng format")
+                    return False
 
-Passer("i")
+        Tokens.append(char.upper())
+        Before = char
+
+    if Depth != 0 or Before in ["&","+","!","("]:
+        print("Wrong format: incomplete expression")
+        return False
+    else:
+        return True
+
+def StartTable():
+    global Tokens; global Vars
+    NumRows = 2 ** len(Vars)
+    for i in range(NumRows):
+        #Generates the corresponding binary values for each row and stores them in a dictionary
+        RowVals = f"{i:0{len(Vars)}b}"
+        CurrentVals = dict(zip(Vars, RowVals))
+        
+
+Passer()
 print(Vars)
+print(Tokens)
+StartTable()
